647681b88c70dbdba836af55d3085c3675bc9a4e
aeski08 <60294078+aeski08@users.noreply.github.com>
2023-05-25 17:27:47 +0100

    First

--- src/test/java/stepdefinitions/US_001_StepDef_AO.java
+++ src/test/java/stepdefinitions/US_001_StepDef_AO.java
@@ -1,5 +1,6 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
@@ -7,6 +8,7 @@ import pages.HomePage_AO;
 import utilities.BrowserUtilities;
 import utilities.ConfigurationReader;
 import utilities.Driver;
+import utilities.JSUtils;
 
 public class US_001_StepDef_AO {
 
@@ -16,8 +18,8 @@ public class US_001_StepDef_AO {
     @Given("AO goes to home page")
     public void AOGoesToHomePage() {
 
-      // Driver.getDriver().get("https://test.hypnotes.net/");
-       Driver.getDriver().get(ConfigurationReader.getProperty("hypnotesUrl"));
+        // Driver.getDriver().get("https://test.hypnotes.net/");
+        Driver.getDriver().get(ConfigurationReader.getProperty("hypnotesUrl"));
 
     }
 
@@ -26,7 +28,7 @@ public class US_001_StepDef_AO {
 
         String expectedUrl = "https://test.hypnotes.net/";
         String actualUrl = Driver.getDriver().getCurrentUrl();
-        Assert.assertEquals(expectedUrl,actualUrl);
+        Assert.assertEquals(expectedUrl, actualUrl);
 
         System.out.println("expectedUrl = " + expectedUrl);
         System.out.println("actualUrl = " + actualUrl);
@@ -35,6 +37,7 @@ public class US_001_StepDef_AO {
         System.out.println("actualTitle = " + actualTitle);
 
     }
+
     @Then("AO verifies headings are visible in the home page")
     public void aoVerifiesHeadingsAreVisibleInTheHomePage() {
         BrowserUtilities.waitFor(3);
@@ -45,6 +48,7 @@ public class US_001_StepDef_AO {
             Assert.assertTrue(homePage_ao.allHeadings.get(i).isDisplayed());
         }
     }
+
     @Then("AO verifies headings are clickable in the home page")
     public void aoVerifiesHeadingsAreClickableInTheHomePage() {
         for (int i = 0; i < homePage_ao.allHeadings.size(); i++) {
@@ -52,6 +56,7 @@ public class US_001_StepDef_AO {
             Assert.assertTrue(homePage_ao.allHeadings.get(i).isEnabled());
         }
     }
+
     @Then("AO verifies try it for Free is visible and clickable")
     public void aoVerifiesTryItForFreeIsVisibleAndClickable() {
 
@@ -59,9 +64,68 @@ public class US_001_StepDef_AO {
         Assert.assertTrue(homePage_ao.button_tryItForFree.isDisplayed());
         Assert.assertTrue(homePage_ao.button_tryItForFree.isEnabled());
     }
+
     @Then("AO verifies Sign In headers is visible and clickable")
     public void aoVerifiesSignInHeadersIsVisibleAndClickable() {
 
+        Assert.assertTrue(homePage_ao.link_signIn.isDisplayed());
+        Assert.assertTrue(homePage_ao.link_signIn.isEnabled());
+
+        homePage_ao.link_signIn.click();
+        BrowserUtilities.waitFor(2);
+
+        String expectedUrl = "https://test.hypnotes.net/login";
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+        Assert.assertEquals(expectedUrl, actualUrl);
+
+        System.out.println("expectedUrl = " + expectedUrl);
+        System.out.println("actualUrl = " + actualUrl);
+
+        String expectedTitle = "Login";
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals(expectedTitle, actualTitle);
+        System.out.println("expectedTitle = " + expectedTitle);
+        System.out.println("actualTitle = " + actualTitle);
+
+    }
+
+    @And("AO goes to at the button of home page")
+    public void aoGoesToAtTheButtonOfHomePage() {
+        //JSUtils.scrollDownByJS();
+        BrowserUtilities.waitFor(2);
+    }
+
+    @Then("AO verifies Go ahead, give it a try! text is visible")
+    public void aoVerifiesGoAheadGiveItATryTextIsVisible() {
+        Assert.assertTrue(homePage_ao.text_goAhead.isDisplayed());
+        System.out.println("homePage_ao = " + homePage_ao.text_goAhead.getText());
+    }
+
+    @Then("Try it for Free button should be visible and clickable.")
+    public void tryItForFreeButtonShouldBeVisibleAndClickable() {
+        Assert.assertTrue(homePage_ao.button_tryItForFreeBottom.isDisplayed());
+        Assert.assertTrue(homePage_ao.button_tryItForFreeBottom.isEnabled());
+    }
+
+    @And("AO clicks the Try it for Free button")
+    public void aoClicksTheTryItForFreeButton() {
+        JSUtils.scrollIntoViewJS(homePage_ao.button_tryItForFreeBottom);
+        BrowserUtilities.waitFor(2);
+        BrowserUtilities.clickWithJS(homePage_ao.button_tryItForFreeBottom);
+        BrowserUtilities.waitFor(2);
+    }
+
+    @Then("Therapist Register section should be visible.")
+    public void therapistRegisterSectionShouldBeVisible() {
+        Assert.assertTrue(homePage_ao.text_registerAsaTerapist.isDisplayed());
+    }
+
+    @Then("AO verifies Related page url is {string}")
+    public void aoVerifiesRelatedPageUrlIs(String arg0) {
+        String expectedUrl = "https://test.hypnotes.net/register";
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+
+        Assert.assertEquals(expectedUrl, actualUrl);
 
     }
 }
--- src/test/resources/features/US_001_02_03_04_AO.feature
+++ src/test/resources/features/US_001_02_03_04_AO.feature
@@ -1,35 +1,41 @@
 @US_001
 Feature: US_001 As a user I should be able to access the home page.
 
+  Background:
+  Given AO goes to home page
+
   Scenario: US_001_TC001 The home page must be accessible when the URL is entered
 
 
-    Given AO goes to home page
+   // Given AO goes to home page
     Then AO verifies the url
 
   Scenario: US_002_TC001 Blog, Services, Solutions, Pricing, Contact us, About us, ?,
   Login, Sign Up headings should be visible and clickable.
 
-    Given AO goes to home page
+    // Given AO goes to home page
     Then AO verifies headings are visible in the home page
     Then AO verifies headings are clickable in the home page
 #helllooo
 
   Scenario: US_003_TC001 Try it for Free, Sign In headers should be visible and clickable.
 
-    Given AO goes to home page
+    // Given AO goes to home page
     Then AO verifies try it for Free is visible and clickable
     Then AO verifies Sign In headers is visible and clickable
-
-
-
-
-
 
 
   Scenario: US_003_TC002 At the bottom of the page, Go ahead, give it a try! and Try it for Free should be visible and clickable.
                          After clicking Therapist Register section should be visible.
                          Related page url must be: https://test.hypnotes.net/register
+    // Given AO goes to home page
+    And AO goes to at the button of home page
+    Then AO verifies Go ahead, give it a try! text is visible
+    Then Try it for Free button should be visible and clickable.
+    And AO clicks the Try it for Free button
+    Then Therapist Register section should be visible.
+    Then AO verifies Related page url is "https://test.hypnotes.net/register"
+
 
 
 
